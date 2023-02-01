f4a282b049b930289c34023059be89162fed5eb0
sahishnusuresh <sahishnusuresh@gmail.com>
2023-02-01 12:01:12 +0530

    changes

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,121 +1,59 @@
 import {
-  StackContext,
   Api,
   Cognito,
+  StackContext,
   ViteStaticSite,
 } from "@serverless-stack/resources";
-import * as cognito from "aws-cdk-lib/aws-cognito";
 
 export function MyStack({ stack, app }: StackContext) {
+  // Create User Pool
   const auth = new Cognito(stack, "Auth", {
-    cdk: {
-      userPoolClient: {
-        supportedIdentityProviders: [
-          {
-            name: "GitHub",
-          },
-        ],
-        oAuth: {
-          callbackUrls: [
-            app.stage === "prod"
-              ? "https://my-app.com"
-              : "http://localhost:3000",
-          ],
-          logoutUrls: [
-            app.stage === "prod"
-              ? "https://my-app.com"
-              : "https://localhost:3000",
-          ],
+    login: ["email"],
+  });
+
+  // Create Api
+  const api = new Api(stack, "Api", {
+    authorizers: {
+      jwt: {
+        type: "user_pool",
+        userPool: {
+          id: auth.userPoolId,
+          clientIds: [auth.userPoolClientId],
         },
       },
     },
-  });
-  // Create a HTTP API
-const api = new Api(stack, "api", {
-  authorizers: {
-    userPool: {
-      type: "user_pool",
-      userPool: {
-        id: auth.userPoolId,
-        clientIds: [auth.userPoolClientId],
+    defaults: {
+      authorizer: "jwt",
+    },
+    routes: {
+      "GET /private": "functions/private.main",
+      "GET /public": {
+        function: "functions/public.main",
+        authorizer: "none",
       },
     },
-  },
-  defaults: {
-    authorizer: "none",
-  },
-  routes: {
-    "GET /public": "functions/public.handler",
-    "GET /user": "functions/user.handler",
-    "POST /token": "functions/token.handler",
-    "GET /private": {
-      function: "functions/private.handler",
-      authorizer: "userPool",
-    },
-  },
-});
-// Throw error if client ID & secret are not provided
-if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET)
-  throw new Error("Please set GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET");
+  });
 
-// Create a GitHub OIDC IDP
-const idp = new cognito.CfnUserPoolIdentityProvider(
-  stack,
-  "GitHubIdentityProvider",
-  {
-    providerName: "GitHub",
-    providerType: "OIDC",
-    userPoolId: auth.userPoolId,
-    providerDetails: {
-      client_id: process.env.GITHUB_CLIENT_ID,
-      client_secret: process.env.GITHUB_CLIENT_SECRET,
-      attributes_request_method: "GET",
-      oidc_issuer: "https://github.com",
-      authorize_scopes: "openid user",
-      authorize_url: "https://github.com/login/oauth/authorize",
-      token_url: api.url + "/token",
-      attributes_url: api.url + "/user",
-      jwks_uri: api.url + "/token",
+  // allowing authenticated users to access API
+  auth.attachPermissionsForAuthUsers(stack, [api]);
+  const site = new ViteStaticSite(stack, "Site", {
+    path: "frontend",
+    environment: {
+      VITE_APP_API_URL: api.url,
+      VITE_APP_REGION: app.region,
+      VITE_APP_USER_POOL_ID: auth.userPoolId,
+      VITE_APP_USER_POOL_CLIENT_ID: auth.userPoolClientId,
     },
-    attributeMapping: {
-      email: "email",
-      name: "name",
-      picture: "avatar_url",
-    },
-  }
-);
+  });
+  
+  // Show the API endpoint and other info in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+    UserPoolId: auth.userPoolId,
+    UserPoolClientId: auth.userPoolClientId,
+    SiteUrl: site.url,
+  });
 
-// attach the IDP to the client
-if (idp) {
-  auth.cdk.userPoolClient.node.addDependency(idp);
-}
-// Create a cognito userpool domain
-const domain = auth.cdk.userPool.addDomain("AuthDomain", {
-  cognitoDomain: {
-    domainPrefix: `${app.stage}-github-demo-oauth`,
-  },
-});
-// Allow authenticated users invoke API
-auth.attachPermissionsForAuthUsers(stack, [api]);
-// Create a React Static Site
-const site = new ViteStaticSite(stack, "Site", {
-  path: "frontend",
-  environment: {
-    VITE_APP_COGNITO_DOMAIN: domain.domainName,
-    VITE_APP_STAGE: app.stage,
-    VITE_APP_API_URL: api.url,
-    VITE_APP_REGION: app.region,
-    VITE_APP_USER_POOL_ID: auth.userPoolId,
-    VITE_APP_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId,
-    VITE_APP_USER_POOL_CLIENT_ID: auth.userPoolClientId,
-  },
-});
+  
 
-// Show the endpoint in the output
-stack.addOutputs({
-  api_endpoint: api.url,
-  auth_client_id: auth.userPoolClientId,
-  domain: domain.domainName,
-  site_url: site.url,
-});
 }
